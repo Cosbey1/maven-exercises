aebafca662cfc680bdfc3a7c23ba149697fd1c5f
CleoBreaux <corey.osbey@gmail.com>
2022-10-20 09:15:20 -0500

    maven exercise

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       String test = StringUtils.upperCase("Lowercasetouppercase");
+        System.out.println(test);
+
+      class Interact {
+          public static void main(String[] args) {
+              Scanner scanner = new Scanner(System.in);
+              System.out.println("Enter something!");
+              String test2 = StringUtils.reverse("");
+              System.out.println(test2);
+          }
+
+      }
+//    int userInput;
+
+    }
+
+}
